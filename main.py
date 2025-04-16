cf607d41b18783dbd9f85e792183c39de41d8721
krutoipasan <grugorsergey@gmail.com>
2025-04-16 22:01:12 +0300

    main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QListWidget, QFileDialog, QHBoxLayout, QVBoxLayout
 
 app=QApplication([])
 win=QWidget()
+win.resize(700, 500)
+win.setWindowTitle("Easy Editor")
 
 btn_file=QPushButton("Папка")
 btn_left=QPushButton("Вліво")
@@ -11,7 +13,9 @@ btn_mirror=QPushButton("Відзеркалити")
 btn_intensity=QPushButton("Різкість")
 btn_wb=QPushButton("Ч\Б")
 
-list=QFileDialog()
+label=QLabel("Картина")
+
+thelist=QListWidget()
 
 mainlayout=QHBoxLayout()
 
@@ -23,8 +27,18 @@ Hlayout2=QHBoxLayout()
 Hlayout3=QHBoxLayout()
 
 Vlayout1.addWidget(btn_file)
-Vlayout1.addWidget(list)
+Vlayout1.addWidget(thelist)
 
+Hlayout2.addWidget(label, 95)
+Hlayout3.addWidget(btn_left)
+Hlayout3.addWidget(btn_right)
+Hlayout3.addWidget(btn_mirror)
+Hlayout3.addWidget(btn_intensity)
+Hlayout3.addWidget(btn_wb)
+
+Vlayout2.addLayout(Hlayout1)
+Vlayout2.addLayout(Hlayout2)
+Vlayout2.addLayout(Hlayout3)
 
 mainlayout.addLayout(Vlayout1)
 mainlayout.addLayout(Vlayout2)
